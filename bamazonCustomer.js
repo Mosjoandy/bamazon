e3d5988754056c452dc5bbc73953bda573086dc9
Mosjoandy <34078510+Mosjoandy@users.noreply.github.com>
2018-05-26 01:25:54 -0700

    made inquirer work with help of mel, attempt at updating db quantity

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -21,24 +21,46 @@ connection.query("SELECT * FROM inventory", function(err, res) {
     for (var i = 0; i < res.length; i++) {
     console.log("Product: " + res[i].product_name + " || Price: $" + res[i].product_price + " || Stock: " + res[i].product_quantity + " || ID: " + res[i].id + "\n");
     }
+
     inquirer.prompt([
         {
         name: "answer1",
         type: "input",
         message: "To purchase, enter product ID"
+        },
+        {
+        name: "answer2",
+        type: "input",
+        message: "How many desired?"
         }
     ]).then(answers => {
-        console.log("You input ID: " + answers.answer1)
         var chosenProduct;
+        var chosenQuantity;
         for (var i = 0; i < res.length; i++) {
-            if (answers.answer1 ===  res[i].id) {
-                chosenProduct = res[i].id;   
-              
+            if (parseInt(answers.answer1) ===  res[i].id) {
+                chosenProduct = res[i].product_name;
+                chosenQuantity = res[i].product_quantity;
             }
-            
         }
-        console.log("You want to buy " + chosenProduct);
-      
+        console.log("Product ID: " + answers.answer1);
+        console.log("Buy: " + chosenProduct);
+        console.log("Stock: " + chosenQuantity);
+        console.log("====================Processing====================");
+                connection.query("UPDATE product_quantity SET ? WHERE ?",
+                [
+                {
+                    product_quantity: (product_quantity - chosenQuantity)
+                },
+                {
+                    product_name: chosenProduct
+                }
+                ], function (err, res) {
+                    console.log("====================Completed Transaction====================");
+                    console.log("Product: " + chosenProduct);
+                    console.log("Stock: " + res[i].product_quantity);
+                }
+            );
+        
     });
 
 //end of the line
